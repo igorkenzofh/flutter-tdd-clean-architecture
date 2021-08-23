2961a8332e452bca0b4aa015c55ff84dcc33a36b
igorkenzofh <igor.hayakawa@gmail.com>
2021-08-23 14:18:53 -0300

    ensure httpadapter throw ServerError on 500

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -92,5 +92,21 @@ void main() {
 
       expect(future, throwsA(HttpError.badRequest));
     });
+
+    test('should return BadRequestError if post returns 400', () async {
+      mockResponse(400, body: '');
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('should return ServerError if post returns 500', () async {
+      mockResponse(500);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
   });
 }
