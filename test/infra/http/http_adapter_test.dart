3da999adaa5f8d1d9e664178403aa5d668529db6
igorkenzofh <igor.hayakawa@gmail.com>
2021-08-23 11:18:02 -0300

    ensure httpadapter returns null if post returns 204 with data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -21,7 +21,11 @@ class HttpAdapter {
     final jsonBody = body != null ? jsonEncode(body) : null;
     final response =
         await client.post(Uri.parse(url), headers: headers, body: jsonBody);
-    return response.body.isEmpty ? null : jsonDecode(response.body) as Map;
+    if (response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body) as Map;
+    } else {
+      return null;
+    }
   }
 }
 
@@ -90,6 +94,14 @@ void main() {
 
     test('should return null if post returns 204', () async {
       mockResponse(204, body: '');
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
+
+    test('should return null if post returns 204 with data', () async {
+      mockResponse(204);
 
       final response = await sut.request(url: url, method: 'post');
 
