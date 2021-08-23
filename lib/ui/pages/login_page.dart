2f987939994908eaced7582cd14b1435f5f6ee79
igorkenzofh <igor.hayakawa@gmail.com>
2021-08-23 15:59:40 -0300

    add LoginPage layout

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../components/components.dart';
 
 class LoginPage extends StatelessWidget {
   @override
@@ -6,62 +7,79 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              child: Image(image: AssetImage('lib/ui/assets/logoex.png')),
+            LoginHeader(),
+            Headline1(
+              text: 'Login',
             ),
-            Text('Login'),
-            Form(
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      icon: Icon(Icons.email),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        icon: Icon(
+                          Icons.email,
+                          color: Theme.of(context).primaryColorLight,
+                        ),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
                     ),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Senha',
-                      icon: Icon(Icons.lock),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0, bottom: 32),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Senha',
+                          icon: Icon(Icons.lock,
+                              color: Theme.of(context).primaryColorLight),
+                        ),
+                        obscureText: true,
+                      ),
                     ),
-                    obscureText: true,
-                  ),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(4),
-                    child: Stack(
-                      children: [
-                        Positioned.fill(
-                          child: Container(
-                            decoration: const BoxDecoration(
-                              gradient: LinearGradient(
-                                colors: <Color>[
-                                  Color(0xFF0D47A1),
-                                  Color(0xFF1976D2),
-                                  Color(0xFF42A5F5),
-                                ],
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Stack(
+                        children: [
+                          Positioned.fill(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                    begin: Alignment.topRight,
+                                    end: Alignment.bottomLeft,
+                                    colors: [
+                                      Theme.of(context).primaryColorLight,
+                                      Theme.of(context).primaryColorDark
+                                    ]),
                               ),
                             ),
                           ),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(16.0),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(fontSize: 20),
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 16.0, vertical: 2),
+                            ),
+                            onPressed: () {},
+                            child: Text('Entrar',
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white)),
                           ),
-                          onPressed: () {},
-                          child: const Text('Entrar'),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  TextButton.icon(
-                      onPressed: () {},
-                      icon: Icon(Icons.person),
-                      label: Text('Criar conta'))
-                ],
+                    TextButton.icon(
+                        onPressed: () {},
+                        icon: Icon(Icons.person,
+                            color: Theme.of(context).primaryColorLight),
+                        label: Text(
+                          'Criar conta',
+                          style:
+                              TextStyle(color: Theme.of(context).primaryColor),
+                        ))
+                  ],
+                ),
               ),
             )
           ],
